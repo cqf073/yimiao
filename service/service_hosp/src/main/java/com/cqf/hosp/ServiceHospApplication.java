bc1971bbdd7470edd117c9b229bf9fa858197284
cqf073 <1730581444@qq.com>
2023-01-02 13:16:09 +0800

    finish pojo

---
+++ service/service_hosp/src/main/java/com/cqf/hosp/ServiceHospApplication.java
@@ -0,0 +1,20 @@
+package com.cqf.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能描述：
+ *
+ * @Author：cqf
+ * @2023/1/115:00
+ * @description： TODO
+ * @modifiedBy：
+ * @version: 1.0
+ **/
+@SpringBootApplication
+public class ServiceHospApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospApplication.class,args);
+    }
+}
